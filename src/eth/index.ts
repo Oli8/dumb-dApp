863ea5e09152019c014392e08fc7f738aeddc483
Olivier Crété <olivier.crete@epitech.eu>
2021-05-31 18:24:45 +0200

    clean up eth

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -4,67 +4,51 @@ import { utils } from 'ethers';
 import type adress from '../types/adress';
 
 export async function connectWallet() {
-  if ('ethereum' in window) {
-    try {
-      const [account] = await window.ethereum.request({
-        method: 'eth_requestAccounts'
-      });
-      return account;
-    } catch (error) {
-      return false;
-    }
+  try {
+    const [account] = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    });
+    return account;
+  } catch (error) {
+    return false;
   }
-
-  return false;
 }
 
 export async function getConnectedWallet() {
-  if ('ethereum' in window) {
-    try {
-      const accounts = await window.ethereum.request({
-        method: 'eth_accounts'
-      });
+  try {
+    const [account] = await window.ethereum.request({
+      method: 'eth_accounts'
+    });
 
-      return accounts[0];
-    } catch (error) {
-      return false;
-    }
+    return account;
+  } catch (error) {
+    return false;
   }
-
-  return false;
 }
 
 export async function getEthBalance(account: adress) {
-  if ('ethereum' in window) {
-    try {
-      const balance = await window.ethereum.request({
-        method: 'eth_getBalance',
-        params: [account]
-      });
+  try {
+    const balance = await window.ethereum.request({
+      method: 'eth_getBalance',
+      params: [account]
+    });
 
-      return Number(utils.formatEther(balance));
-    } catch (error) {
-      return false;
-    }
+    return Number(utils.formatEther(balance));
+  } catch (error) {
+    return false;
   }
-
-  return false;
 }
 
 export async function getChain() {
-  if ('ethereum' in window) {
-    try {
-      const chainId = await window.ethereum.request({
-        method: 'eth_chainId',
-      });
+  try {
+    const chainId = await window.ethereum.request({
+      method: 'eth_chainId',
+    });
 
-      return chainId;
-    } catch (error) {
-      return false;
-    }
+    return chainId;
+  } catch (error) {
+    return false;
   }
-
-  return false;
 }
 
 export async function sendTransaction({from, to, amount}: {
@@ -72,24 +56,20 @@ export async function sendTransaction({from, to, amount}: {
   to: adress,
   amount: number
 }) {
-  if ('ethereum' in window) {
-    try {
-      const weiAmount = utils.parseEther(String(amount))
-                             .toHexString()
-      const tx = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [{
-          from,
-          to,
-          value: weiAmount,
-        }]
-      });
+  try {
+    const weiAmount = utils.parseEther(String(amount))
+                           .toHexString()
+    const tx = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [{
+        from,
+        to,
+        value: weiAmount,
+      }]
+    });
 
-      return tx;
-    } catch (error) {
-      return false;
-    }
+    return tx;
+  } catch (error) {
+    return false;
   }
-
-  return false;
 }
