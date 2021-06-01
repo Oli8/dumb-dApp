59eaae1ca2874064d9c94e270dbec76c6da07d37
Olivier Crété <olivier.crete@epitech.eu>
2021-06-01 18:30:46 +0200

    global classes in tailwind component

--- src/Tailwind.svelte
+++ src/Tailwind.svelte
@@ -1,5 +1,17 @@
 <style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+.box {
+  @apply float-right mr-4 border-dotted border-2 border-blue-400 rounded p-1 relative bottom-1;
+}
+
+.b-input {
+  @apply border block w-full my-3 border block w-full my-3;
+}
+
+.b-button {
+  @apply w-full my-3 block bg-blue-100 uppercase text-lg p-3 rounded;
+}
 </style>
